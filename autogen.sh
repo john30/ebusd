1e9edd3e718b768899735b33199b6451ec8bab8e
Roland Jax <roland.jax@liwest.at>
2014-09-24 16:34:35 +0200

    directory m4 removed; autogen.sh adapted.

--- autogen.sh
+++ autogen.sh
@@ -2,5 +2,7 @@
 test -n "$srcdir" || srcdir=`dirname "$0"`
 test -n "$srcdir" || srcdir=.
 
+mkdir -p "$srcdir/m4"
+
 autoreconf --force --install --verbose "$srcdir"
 test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
