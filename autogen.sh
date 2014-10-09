73f688a830332fdbb5da8eb099420e91e11d693a
Roland Jax <roland.jax@liwest.at>
2014-10-09 16:27:32 +0200

    Version changed from 0.3.0 to 0.4.0

--- autogen.sh
+++ autogen.sh
@@ -3,6 +3,7 @@ test -n "$srcdir" || srcdir=`dirname "$0"`
 test -n "$srcdir" || srcdir=.
 
 mkdir -p "$srcdir/m4"
+mkdir -p "$srcdir/build"
 
 autoreconf --force --install --verbose "$srcdir"
 test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
