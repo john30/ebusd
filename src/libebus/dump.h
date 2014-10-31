02b8b922492ef5f0cd8bf8ee8acb2a1ee74d2841
Roland Jax <roland.jax@liwest.at>
2014-10-31 15:42:48 +0100

    documentation for class Dump added.

--- src/libebus/dump.h
+++ src/libebus/dump.h
@@ -25,21 +25,44 @@
 namespace libebus
 {
 
-
+/**
+ * @brief Class for writing raw bytes to binary file.
+ */
 class Dump
 {
 
 public:
+	/**
+	 * @brief Create a new instance to write dump files.
+	 * @param filename which will be used for dumping raw bytes.
+	 * @param filesize max. Size of the dump file, before switching.
+	 */
 	Dump(std::string filename, long filesize)
 		: m_filename(filename), m_filesize(filesize) {}
 
+	/**
+	 * @brief write byte to dump file.
+	 * @param byte to write
+	 * @return -1 if dump file cannot opened or renaming of dump file failed.
+	 */
 	int write(const char* byte);
 
+	/**
+	 * @brief setter for dump file name.
+	 * @param filename which will be used for dumping raw bytes.
+	 */
 	void setFilename(const std::string& filename) { m_filename = filename; }
+
+	/**
+	 * @brief setter for max size of dump file.
+	 * @param filesize max. Size of the dump file, before switching.
+	 */
 	void setFilesize(const long filesize) { m_filesize = filesize; }
 
 private:
+	/** the name of dump file*/
 	std::string m_filename;
+	/** max. size of dump file */
 	long m_filesize;
 
 };
