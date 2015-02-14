0c0a137ef5eafccd9d3d53ad6c1147b83bb0029e
john30 <ebusd@johnm.de>
2015-02-14 14:19:53 +0100

    nicer error messages

--- src/lib/ebus/result.cpp
+++ src/lib/ebus/result.cpp
@@ -24,7 +24,7 @@ using namespace std;
 
 const char* getResultCode(result_t resultCode) {
 	switch (resultCode) {
-	case RESULT_OK:               return "success";
+	case RESULT_OK:               return "done";
 	case RESULT_IN_ESC:           return "escape sequence received";
 	case RESULT_SYN:              return "SYN received";
 	case RESULT_EMPTY:            return "empty";
@@ -33,14 +33,14 @@ const char* getResultCode(result_t resultCode) {
 	case RESULT_ERR_SEND:         return "ERR: send error";
 	case RESULT_ERR_ESC:          return "ERR: invalid escape sequence";
 	case RESULT_ERR_TIMEOUT:      return "ERR: read timeout";
-	case RESULT_ERR_NOTFOUND:     return "ERR: file/element not found or not readable";
+	case RESULT_ERR_NOTFOUND:     return "ERR: element not found";
 	case RESULT_ERR_EOF:          return "ERR: end of input reached";
 	case RESULT_ERR_INVALID_ARG:  return "ERR: invalid argument";
 	case RESULT_ERR_INVALID_NUM:  return "ERR: invalid numeric argument";
 	case RESULT_ERR_INVALID_ADDR: return "ERR: invalid address";
 	case RESULT_ERR_INVALID_POS:  return "ERR: invalid position";
 	case RESULT_ERR_OUT_OF_RANGE: return "ERR: argument value out of valid range";
-	case RESULT_ERR_INVALID_PART: return "ERR: invalid part type value";
+	case RESULT_ERR_INVALID_PART: return "ERR: invalid part type";
 	case RESULT_ERR_MISSING_TYPE: return "ERR: missing data type";
 	case RESULT_ERR_INVALID_LIST: return "ERR: invalid value list";
 	case RESULT_ERR_DUPLICATE:    return "ERR: duplicate entry";
@@ -51,7 +51,7 @@ const char* getResultCode(result_t resultCode) {
 	case RESULT_ERR_NO_SIGNAL:    return "ERR: no signal";
 	default:
 		if (resultCode >= 0)
-			return "success: unknown result code";
+			return "done: unknown result code";
 		return "ERR: unknown result code";
 	}
 }
