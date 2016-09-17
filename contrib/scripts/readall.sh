d7d667b189559a7e2c5bdfdf2095c8be02c93a69
john30 <ebusd@ebusd.eu>
2016-09-17 16:20:55 +0200

    allow definition of port to connect to

--- contrib/scripts/readall.sh
+++ contrib/scripts/readall.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
-for i in `echo "f" "$@"|nc localhost 8888|cut -d ' ' -f '1-2'|sed -e 's# #:#'`; do
-  ret=`echo "r -c" ${i%%:*} ${i##*:}|nc localhost 8888|head -n 1`
+port=8888
+if [ "x$1" = "x-p" ]; then
+  shift
+  port=$1
+  shift
+fi
+for i in `echo "f" "$@"|nc localhost $port|cut -d ' ' -f '1-2'|sed -e 's# #:#'`; do
+  ret=`echo "r -c" ${i%%:*} ${i##*:}|nc localhost $port|head -n 1`
   echo ${i%%:*} ${i##*:} "=" $ret
 done
