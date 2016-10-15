62ea2f321345d88323a6e3303271f9b12f241368
john30 <ebusd@ebusd.eu>
2016-10-15 17:51:11 +0200

    allow passing arguments to read command wiht -R "args"

--- contrib/scripts/readall.sh
+++ contrib/scripts/readall.sh
@@ -5,7 +5,13 @@ if [ "x$1" = "x-p" ]; then
   port=$1
   shift
 fi
+readargs=
+if [ "x$1" = "x-R" ]; then
+  shift
+  readargs=$1
+  shift
+fi
 for i in `echo "f" "$@"|nc localhost $port|cut -d ' ' -f '1-2'|sed -e 's# #:#'`; do
-  ret=`echo "r -c" ${i%%:*} ${i##*:}|nc localhost $port|head -n 1`
+  ret=`echo "r ${readargs} -c" ${i%%:*} ${i##*:}|nc localhost $port|head -n 1`
   echo ${i%%:*} ${i##*:} "=" $ret
 done
