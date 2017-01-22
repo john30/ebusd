caa8fa6ca4ae8fc98ce240b01a2b3cf071f2b852
john30 <ebusd@ebusd.eu>
2017-01-22 13:58:12 +0100

    added cmake support

---
+++ config.h.cmake
@@ -0,0 +1,56 @@
+/* Defined if contributed sources are enabled. */
+#cmakedefine HAVE_CONTRIB
+
+/* Defined if direct conversion from float to int is available (2 for swapped byte order). */
+#cmakedefine HAVE_DIRECT_FLOAT_FORMAT ${HAVE_DIRECT_FLOAT_FORMAT}
+
+/* Defined if MQTT handling is enabled. */
+#cmakedefine HAVE_MQTT
+
+/* Defined if ppoll() is available. */
+#cmakedefine HAVE_PPOLL
+
+/* Defined if pselect() is available. */
+#cmakedefine HAVE_PSELECT
+
+/* Defined if pthread_setname_np is available. */
+#cmakedefine HAVE_PTHREAD_SETNAME_NP
+
+/* The name of package. */
+#cmakedefine PACKAGE "${PACKAGE_NAME}"
+
+/* The address where bug reports for this package should be sent. */
+#cmakedefine PACKAGE_BUGREPORT "${PACKAGE_BUGREPORT}"
+
+/* The default path of the configuration files. */
+#cmakedefine PACKAGE_CONFIGPATH "${PACKAGE_CONFIGPATH}"
+
+/* The path and name of the log file. */
+#cmakedefine PACKAGE_LOGFILE "${PACKAGE_LOGFILE}"
+
+/* The full name of this package. */
+#cmakedefine PACKAGE_NAME "${PACKAGE_NAME}"
+
+/* The path and name of the PID file. */
+#cmakedefine PACKAGE_PIDFILE "${PACKAGE_PIDFILE}"
+
+/* The full name and version of this package. */
+#cmakedefine PACKAGE_STRING "${PACKAGE_STRING}"
+
+/* The tra name of this package. */
+#cmakedefine PACKAGE_TARNAME "${PACKAGE_TARNAME}"
+
+/* The home page for this package. */
+#cmakedefine PACKAGE_URL "${PACKAGE_URL}"
+
+/* The version of this package. */
+#cmakedefine PACKAGE_VERSION "${PACKAGE_VERSION}"
+
+/* The revision of the package. */
+#cmakedefine REVISION "${REVISION}"
+
+/* The version of the package formatted for the scan result. */
+#cmakedefine SCAN_VERSION "${SCAN_VERSION}"
+
+/* The version of the package */
+#cmakedefine VERSION "${VERSION}"
