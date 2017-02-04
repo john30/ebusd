d981fdfb20efb478c1791b6941004094489f4ccd
john30 <ebusd@ebusd.eu>
2017-02-04 12:13:39 +0100

    added MQTT (libmosquitto1), rebuild buildenv only if needed, more verbose

--- contrib/docker/build.sh
+++ contrib/docker/build.sh
@@ -1,18 +1,27 @@
 #!/bin/sh
-set -e
-stamp=`docker inspect -f '{{.Created}}' ebusd-buildenv `
+stamp=`docker inspect -f '{{.Created}}' ebusd-buildenv 2>/dev/null`
 if [ -z "$stamp" ] || ( rm -f .stamp && touch -d "$stamp" .stamp && [ buildenv/Dockerfile -nt .stamp ] ) ; then
-  (cd buildenv && docker build -t ebusd-buildenv .)
+  echo "updating ebusd build environment..."
+  docker rmi -f ebusd-buildenv 2>/dev/null
+  (cd buildenv && docker build -t ebusd-buildenv .) || exit 1
 fi
+set -e
+echo "creating debian image..."
 docker run --rm -it -v `pwd`/../..:/build ebusd-buildenv ./make_debian.sh
 export EBUSD_VERSION=`cat ../../VERSION`
 export EBUSD_ARCH=`docker version|grep -i "Arch[^:]*server"|head -n 1|sed -e 's#^.*/##'`
-mv ../../ebusd-${EBUSD_VERSION}_${EBUSD_ARCH}.deb runtime/
+mv ../../ebusd-${EBUSD_VERSION}_${EBUSD_ARCH}_mqtt1.deb runtime/
+echo "building docker image..."
 (cd runtime && docker build -t ebusd .)
-echo "docker image created."
-echo
-echo "to run it interactively on serial device /dev/ttyUSB1:"
-echo "docker run --rm -it --device=/dev/ttyUSB1:/dev/ttyUSB0 -p 127.0.0.1:8888:8888 ebusd"
-echo
-echo "to start it in background on serial device /dev/ttyUSB1:"
-echo "docker run device=/dev/ttyUSB1:/dev/ttyUSB0 -p 127.0.0.1:8888:8888 ebusd"
+cat <<EOF
+docker image created.
+
+to run it interactively on serial device /dev/ttyUSB1:
+docker run --rm -it --device=/dev/ttyUSB1:/dev/ttyUSB0 -p 127.0.0.1:8888:8888 ebusd
+
+to start it in background on serial device /dev/ttyUSB1:
+docker run device=/dev/ttyUSB1:/dev/ttyUSB0 -p 127.0.0.1:8888:8888 ebusd
+
+when using a network device, the "--device" argument to docker can be omitted:
+docker run --rm -it -p 127.0.0.1:8888:8888 ebusd -f --scanconfig -d udp:192.168.178.123:10000 --latency=80000
+EOF
