5a23e5c13a417bc23e5aa94b55d58af365aab34b
john30 <ebusd@ebusd.eu>
2017-03-05 13:14:38 +0100

    unix mode

--- contrib/updatecheck/calcversions.sh
+++ contrib/updatecheck/calcversions.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 version=`head -n 1 VERSION`
 revision=`git describe --always`
-echo "ebusd=${version}.${revision}"
+echo "ebusd=${version}.${revision}" > versions.txt
 #TODO add calculation for config files
