46f60e821c4b62d7e83b7115958bb0bc0e4559c0
john30 <ebusd@ebusd.eu>
2017-08-27 16:46:41 +0200

    fixed testing

--- src/lib/utils/thread.h
+++ src/lib/utils/thread.h
@@ -151,22 +151,34 @@ class Mutex {
   /**
    * Constructor.
    */
-  Mutex();
+  inline Mutex() {
+    pthread_mutexattr_t attr;
+    pthread_mutexattr_init(&attr);
+    pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_RECURSIVE);
+    pthread_mutex_init(&m_mutex, &attr);
+    pthread_mutexattr_destroy(&attr);
+  }
 
   /**
    * Destructor.
    */
-  virtual ~Mutex();
+  virtual inline ~Mutex() {
+    pthread_mutex_destroy(&m_mutex);
+  };
 
   /**
    * Lock this mutex.
    */
-  void lock();
+  void inline lock() {
+    pthread_mutex_lock(&m_mutex);
+  }
 
   /**
    * Unlock this mutex.
    */
-  void unlock();
+  void inline unlock() {
+    pthread_mutex_unlock(&m_mutex);
+  }
 
  private:
   /** the mutex for waiting. */
