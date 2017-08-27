514aa1c9671f35f4427968ecc5414f94a3049061
john30 <ebusd@ebusd.eu>
2017-08-27 13:33:25 +0200

    test socket_create_listen

--- test_coverage.sh
+++ test_coverage.sh
@@ -88,23 +88,15 @@ php -r '
 error_reporting(E_ALL);
 set_time_limit(0);
 ob_implicit_flush();
-if (($srv=socket_create(AF_INET, SOCK_STREAM, SOL_TCP))===false) {
-  die("server: create socket");
-}
-if (socket_bind($srv, "127.0.0.1", 18876)===false) {
-  @socket_close($srv);
-  die("server: bind socket");
-}
-echo "server: listen\n";
-if (socket_listen($srv, 5)===false) {
-  @socket_close($srv);
-  die("server: listen socket");
+if (($srv=socket_create_listen(18876))===false) {
+  die("server: create_listen socket");
 }
 echo "server: waiting\n";
 if (($cli=socket_accept($srv))===false) {
   @socket_close($srv);
   die("server: socket accept");
 }
+echo "server: connection accepted\n";
 socket_set_block($cli);
 $output="";
 $input=$hexinput="";
