29cee10ff54708a347574080055de46d3f23ac01
john30 <ebusd@ebusd.eu>
2017-08-29 23:19:48 +0200

    added helper script

---
+++ contrib/scripts/readallvaillantregisters.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+port=8888
+if [ "x$1" = "x-p" ]; then
+  shift
+  port=$1
+  shift
+fi
+addr=08
+if [ "x$1" = "x-a" ]; then
+  shift
+  addr=$1
+  shift
+fi
+for (( i=0; i<512; i++ )) ; do
+  h=`printf "%4.4X" $i`
+  ret=`echo "hex ${addr}b509030d${h##??}${h%%??}"|nc localhost $port|head -n 1`
+  echo $i "=" $ret
+done
