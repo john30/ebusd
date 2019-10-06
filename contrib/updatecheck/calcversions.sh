20a8f4f7f650910ef7c493d40235b7e884c202d0
john30 <ebusd@ebusd.eu>
2019-10-06 11:25:33 +0200

    distinguish between problematic binaries before commit 94932c1 and after

--- contrib/updatecheck/calcversions.sh
+++ contrib/updatecheck/calcversions.sh
@@ -4,3 +4,4 @@ revision=`git describe --always`
 echo "ebusd=${version},${revision}" > versions.txt
 files=`find config/ -type f -or -type l`
 ../../src/lib/ebus/test/test_filereader $files|sed -e 's#^config/##' -e 's#^\([^ ]*\) #\1=#' -e 's# #,#g'|sort >> versions.txt
+./oldtest_filereader $files|sed -e 's#^config/##' -e 's#^\([^ ]*\) #\1=#' -e 's# #,#g'|sort >> oldversions.txt
