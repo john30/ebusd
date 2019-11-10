f55101d9a681a84112e3020a5bb77158013b2c1f
john30 <ebusd@ebusd.eu>
2019-11-10 19:30:35 +0100

    update rpi as well

--- contrib/docker/update.sh
+++ contrib/docker/update.sh
@@ -2,5 +2,5 @@
 set -e
 export EBUSD_VERSION=`cat ../../VERSION`
 export EBUSD_ARCH=`docker version|grep -i "Arch[^:]*:"|tail -n 1|sed -e 's#^.*/##'`
-sed -i -e "s#^ENV EBUSD_VERSION .*\$#ENV EBUSD_VERSION ${EBUSD_VERSION}#" -e "s#^ENV EBUSD_ARCH .*\$#ENV EBUSD_ARCH ${EBUSD_ARCH}#" Dockerfile release/Dockerfile
+sed -i -e "s#^ENV EBUSD_VERSION .*\$#ENV EBUSD_VERSION ${EBUSD_VERSION}#" -e "s#^ENV EBUSD_ARCH .*\$#ENV EBUSD_ARCH ${EBUSD_ARCH}#" Dockerfile release/Dockerfile rpirelease/Dockerfile
 
