385f4e2abd142872e0470f6aaba37ec9f5e8c645
john30 <ebusd@ebusd.eu>
2021-01-17 19:17:05 +0100

    new process to build all images

--- contrib/docker/build.sh
+++ contrib/docker/build.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
-stamp=`docker inspect -f '{{.Created}}' ebusd-buildenv 2>/dev/null`
-if [ -z "$stamp" ] || ( rm -f .stamp && touch -d "$stamp" .stamp && [ buildenv/Dockerfile -nt .stamp ] ) ; then
-  echo "updating ebusd build environment..."
-  docker rmi -f ebusd-buildenv 2>/dev/null
-  (cd buildenv && docker build -t ebusd-buildenv .) || exit 1
-fi
-set -e
-echo "creating debian image..."
-docker run --rm -it -v `pwd`/../..:/build ebusd-buildenv ./make_debian.sh
-export EBUSD_VERSION=`cat ../../VERSION`
-export EBUSD_ARCH=`docker version|grep -i "Arch[^:]*:"|tail -n 1|sed -e 's#^.*/##'`
-mv ../../ebusd-${EBUSD_VERSION}_${EBUSD_ARCH}_mqtt1.deb runtime/
-echo "building docker image..."
-(cd runtime && docker build -t ebusd .)
-echo "docker image created."
+archs='amd64 i386 arm32v7 arm64v8'
+UPLOAD_URL='http://'`hostname`'/ebusdreleaseupload.php'
+UPLOAD_CREDENTIALS='anonymous:build'
+for arch in $archs; do
+  if [ "$arch" != "amd64" ]; then
+    suffix=".$arch"
+  else
+    suffix=''
+  fi
+  docker build --build-arg "UPLOAD_URL=$UPLOAD_URL" --build-arg "UPLOAD_CREDENTIALS=$UPLOAD_CREDENTIALS" --build-arg "UPLOAD_ONLY=1" -f release/Dockerfile$suffix .
+done
+
