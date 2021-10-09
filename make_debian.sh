8318e791859f01c4dc99e960c44f6c57530c5d9e
John <ebusd@ebusd.eu>
2021-10-09 17:48:13 +0200

    Update make_debian.sh

--- make_debian.sh
+++ make_debian.sh
@@ -15,6 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+pwd
+ls -la
 keepbuilddir=
 if [ "x$1" = "x--keepbuilddir" ]; then
   keepbuilddir=1
