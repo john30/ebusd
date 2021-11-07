5f59ab9860b45385b3981638666aaa610b6975ac
john30 <ebusd@ebusd.eu>
2021-11-07 15:05:49 +0100

    remove oldversions

--- contrib/updatecheck/calcversions.sh
+++ contrib/updatecheck/calcversions.sh
@@ -5,4 +5,4 @@ echo "ebusd=${version},${revision}" > versions.txt
 echo "ebusd=${version},${revision}" > oldversions.txt
 files=`find config/ -type f -or -type l`
 ../../src/lib/ebus/test/test_filereader $files|sed -e 's#^config/##' -e 's#^\([^ ]*\) #\1=#' -e 's# #,#g'|sort >> versions.txt
-./oldtest_filereader $files|sed -e 's#^config/##' -e 's#^\([^ ]*\) #\1=#' -e 's# #,#g'|sort >> oldversions.txt
+#./oldtest_filereader $files|sed -e 's#^config/##' -e 's#^\([^ ]*\) #\1=#' -e 's# #,#g'|sort >> oldversions.txt
