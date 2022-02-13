4cdac8705a7e8c76e4504089a659ce2614a89fb0
John <ebusd@ebusd.eu>
2022-02-13 21:31:30 +0100

    ignore test for arm for now as crossbuild can't find some files

--- make_debian.sh
+++ make_debian.sh
@@ -96,13 +96,16 @@ if [ -n "$RUNTEST" ]; then
     cat test.txt || true
     exit 1
   }
-  ($RELEASE/usr/bin/ebusd -f -c src/lib/ebus/test -d /dev/null --log=all:debug --inject=stop 10fe0900040000803e/ > test.txt) || testdie "float conversion"
-  egrep "received update-read broadcast test QQ=10: 0\.25$" test.txt || testdie "float result"
   if [ "$RUNTEST" = "full" ]; then
     (cd src/lib/ebus/test && make test_filereader && ./test_filereader) > test.txt || testdie "filereader"
     (cd src/lib/ebus/test && make test_data && ./test_data) > test.txt || testdie "data"
     (cd src/lib/ebus/test && make test_message && ./test_message) > test.txt || testdie "message"
     (cd src/lib/ebus/test && make test_symbol && ./test_symbol) > test.txt || testdie "symbol"
+  fi
+  if [ "${ARCH#*arm}" = "$ARCH" ]; then
+    # only run on non-arm as crossbuild can't read some files
+    ("$RELEASE/usr/bin/ebusd" -f -c src/lib/ebus/test -d /dev/null --log=all:debug --inject=stop 10fe0900040000803e/ > test.txt) || testdie "float conversion"
+    egrep "received update-read broadcast test QQ=10: 0\.25$" test.txt || testdie "float result"
   fi
 fi
 
