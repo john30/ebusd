2c7cab3dc1b14f5bc4f93fa2962f6cfbedf825db
John <ebusd@ebusd.eu>
2023-06-16 20:40:35 +0200

    update year

--- src/lib/utils/queue.h
+++ src/lib/utils/queue.h
@@ -1,6 +1,6 @@
 /*
  * ebusd - daemon for communication with eBUS heating systems.
- * Copyright (C) 2014-2022 John Baier <ebusd@ebusd.eu>
+ * Copyright (C) 2014-2023 John Baier <ebusd@ebusd.eu>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
