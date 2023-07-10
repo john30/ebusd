261f2d45068dc90777b442cea377ea11197bb0b3
Alex Samorukov <samm@os2.kiev.ua>
2023-07-11 00:24:40 +0200

    Fix build on FreeBSD due to missing **environ definition

--- src/ebusd/main.cpp
+++ src/ebusd/main.cpp
@@ -36,6 +36,9 @@
 #include "lib/utils/httpclient.h"
 #include "ebusd/scan.h"
 
+#ifdef __FreeBSD__
+extern char **environ;
+#endif
 
 /** the version string of the program. */
 const char *argp_program_version = "" PACKAGE_STRING "." REVISION "";
