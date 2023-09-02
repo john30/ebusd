bcc952be30680eb3a7778f4ecdc1bc6f07bd9d7b
Serhii Stotskyi <sergiy.stotskiy@gmail.com>
2023-09-02 22:43:39 +0300

    display full hex address and added retry for "read all registers" script

--- contrib/scripts/readallvaillantregisters.sh
+++ contrib/scripts/readallvaillantregisters.sh
@@ -14,5 +14,13 @@ fi
 for (( i=0; i<512; i++ )) ; do
   h=`printf "%4.4X" $i`
   ret=`echo "hex ${addr}b509030d${h##??}${h%%??}"|nc -q 1 localhost $port|head -n 1`
-  echo $i "=" $ret
+  while echo "$ret" | grep -i "ERR:"; do
+    ret=`echo "hex ${addr}b509030d${h##??}${h%%??}"|nc -q 1 localhost $port|head -n 1`
+    retry_count=$((retry_count+1))
+    if [ "$retry_count" = "10" ]; then
+      break;
+    fi
+  done;
+
+  echo "${addr}b509030d${h##??}${h%%??} (index: ${i}) = $ret"
 done
