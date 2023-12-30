303ea9627dd425c52dbb9f6ba8a7ded6b5046f37
John <ebusd@ebusd.eu>
2023-12-30 17:34:23 +0100

    align with upcoming upstream, switch to edge for newest gcc as used in alpine ci

--- contrib/alpine/build-git.sh
+++ contrib/alpine/build-git.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # helper script to check the build from git master is fine
-docker run -it --rm -v $PWD:/build -w /build/contrib/alpine alpine sh -c 'apk add --upgrade abuild && adduser -D test && addgroup test abuild && su test -c "abuild-keygen -a && mkdir git && cd git && cp ../APKBUILD.git APKBUILD && abuild -r checksum && abuild -r"'
+docker run -it --rm -v $PWD:/build -w /build/contrib/alpine alpine:edge sh -c 'apk add --upgrade abuild build-base && adduser -D test && addgroup test abuild && su test -c "abuild-keygen -a && mkdir git && cd git && cp ../APKBUILD.git APKBUILD && abuild -r checksum && abuild -r"'
--- contrib/alpine/build.sh
+++ contrib/alpine/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # helper script to check the release build is fine
-docker run -it --rm -v $PWD:/build -w /build/contrib/alpine alpine sh -c 'apk add --upgrade abuild && adduser -D test && addgroup test abuild && su test -c "abuild-keygen -a && abuild -r"'
+docker run -it --rm -v $PWD:/build -w /build/contrib/alpine alpine:edge sh -c 'apk add --upgrade abuild build-base && adduser -D test && addgroup test abuild && su test -c "abuild-keygen -a && abuild -r"'
